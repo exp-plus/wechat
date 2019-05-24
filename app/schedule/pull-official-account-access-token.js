aaeb73f1aa34a38bcf4016c8bb095d6e4aea3166
kayorl <kayorl@163.com>
2019-05-24 09:47:28 +0800

    定时任务动态启动

--- app/schedule/pull-official-account-access-token.js
+++ app/schedule/pull-official-account-access-token.js
@@ -1,17 +1,21 @@
 'use strict';
 
-exports.schedule = {
-  immediate: true,
-  interval: '20m',
-  type: 'worker',
-};
-
-/**
- * 定时从微信服务器拉取公众号 access_token
- * @param {Object} ctx 上下文对象
- */
-exports.task = async ctx => {
-  const access_token = await ctx.officialAccount.getAccessToken();
-  ctx.app.coreLogger.info('[公众号]\t\t拉取 access_token：' + access_token);
-  ctx.app.messenger.sendToApp('refresh_official_account_access_token', access_token);
+module.exports = app => {
+  return {
+    schedule: {
+      disable: !app.config.officialAccount.agent,
+      immediate: true,
+      interval: '20m',
+      type: 'worker',
+    },
+    /**
+     * 定时从微信服务器拉取公众号 access_token
+     * @param {Object} ctx 上下文对象
+     */
+    async task(ctx) {
+      const access_token = await ctx.officialAccount.getAccessToken();
+      ctx.app.coreLogger.info('[公众号]\t\t拉取 access_token：' + access_token);
+      ctx.app.messenger.sendToApp('refresh_official_account_access_token', access_token);
+    },
+  };
 };
