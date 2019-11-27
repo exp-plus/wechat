11ef4f233d0859cfd4421a720847e4f51865f7ed
kayorl <kayorl@163.com>
2019-11-27 11:33:53 +0800

    增加内容安全

--- test/miniprogram.test.js
+++ test/miniprogram.test.js
@@ -31,6 +31,18 @@ describe('test/miniprogram.test.js', () => {
     });
     assert(image.length > 0);
   });
+
+  it('获取 小程序码', async () => {
+    const ctx = app.mockContext();
+
+    const access_token = await ctx.miniprogram.getAccessToken();
+    const image = await ctx.miniprogram.generateMiniProgramQRCode({
+      access_token,
+      is_hyaline: true,
+      center_image_url: 'https://sqimg.qq.com/qq_product_operations/im/qqlogo/imlogo_b.png',
+    });
+    assert(image.length > 0);
+  });
   // it('手动测试 code2session', done => done());
   // it('手动测试 decryptOpenData', done => done());
   // it('手动测试 sendTemplateMessage', done => done());
